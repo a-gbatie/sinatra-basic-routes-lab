2933e1e076eddeba04257a7f2a00a626a7593c21
a-gbatie <ange.batie@gmail.com>
2020-12-11 10:25:25 -0800

    commit tree ec65258b65f7922f723d38caaeb90d476ff0c939

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+# get '/' do 
+#     "Hello, World!"
+# end
+
+get '/name' do
+    "My name is Ange"
+end
+
+get '/hometown' do
+    "My hometown is Queens"
+end
+
+get '/favorite-song' do
+    "My favorite song is I Have Nothing"
+end
+
 end
